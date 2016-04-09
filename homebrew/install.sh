ce869b405fdb2ad3d475317c9c0a309667c58025
niksmac <nikhil@lightrains.com>
2016-04-09 20:55:29 +0530

    Remved Files

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -18,10 +18,10 @@ then
   then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
   fi
-  
+
 fi
 
 # Install homebrew packages
-brew install grc coreutils spark
+brew install grc coreutils spark tmux youtube-dl
 
 exit 0
