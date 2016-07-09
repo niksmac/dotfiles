4c62955e6c217c57f8f2cb9b24bfed4c80076c81
niksmac <nikhil@lightrains.com>
2016-07-09 11:31:48 +0530

    Added alias just this

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,5 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+
+alias ga='git add'
