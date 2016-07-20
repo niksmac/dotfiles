bc3db72cd3cc7ff2309afac86408bdccef73878e
niksmac <nikhil@lightrains.com>
2016-07-20 22:32:32 +0530

    Added Misc Aliases

---
+++ misc/aliases.zsh
@@ -0,0 +1,11 @@
+# Jekyll Awesomeness
+alias js='jekyll clean && jekyll serve'
+alias jb='jekyll build'
+
+alias c='clear'
+alias yd='youtube-dl'
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias tree='tree -I node_modules'
+
+alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias v='nvim'
