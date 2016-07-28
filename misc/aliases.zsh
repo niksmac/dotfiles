4988842eed29f9b1353020e5f69f32d67283528a
niksmac <nikhil@lightrains.com>
2016-07-28 09:41:52 +0530

    Modified npm global package installations

--- misc/aliases.zsh
+++ misc/aliases.zsh
@@ -8,3 +8,4 @@ alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias tree='tree -I node_modules'
 
 alias v='nvim'
+alias speed='speed-test'
---
+++ npm/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# NPM
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using NPM.
+
+
+# Install homebrew packages
+npm install -g speed-test
+
+exit 0
