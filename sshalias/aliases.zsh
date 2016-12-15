03280bca1d2700b53ca68b75ff653f368226cbf9
niksmac <hello@nikhilmohan.in>
2016-12-15 11:27:10 +0530

    Updates Alias for Godaddy

--- sshalias/aliases.zsh
+++ sshalias/aliases.zsh
@@ -11,5 +11,5 @@
 alias sshpegke='ssh root@128.199.76.62'
 alias sshlight='ssh light@192.168.27.100'
 alias sshksum='ssh root@192.168.52.211'
-alias sshgodady='ssh ligh5599@118.139.175.1'
+alias sshgodady='ssh -oHostKeyAlgorithms=+ssh-dss ligh5599@118.139.175.1'
 alias sshether='ssh root@192.168.27.101'
