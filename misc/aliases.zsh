faffacdf77ba60c432ae8a8841b9ede3cbf46234
niksmac <hello@nikhilmohan.in>
2017-02-22 15:18:37 +0530

    Updated youtbe dl download format

--- misc/aliases.zsh
+++ misc/aliases.zsh
@@ -4,7 +4,7 @@ alias jb='jekyll build'
 alias jsf='jekyll clean && jekyll serve --future'
 
 alias c='clear'
-alias yd='youtube-dl'
+alias yd='youtube-dl -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4'
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias tree='tree -I node_modules'
 
