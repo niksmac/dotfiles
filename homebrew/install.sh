4f65cd0d18c88cdecee15b713420f7a5893e69db
niksmac <nikhil@lightrains.com>
2020-09-25 08:15:09 +0530

    Add cask handbrake

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -29,7 +29,7 @@ then
   # Tap cask
   brew tap caskroom/cask
   # Install cask items
-  brew cask install flux transmission vlc gpg-suite iterm2 brave-browser
+  brew cask install flux transmission vlc gpg-suite iterm2 brave-browser handbrake
 elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
 then
   # Install homebrew packages
