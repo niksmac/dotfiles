622f86414d69521e310ac56eae939dc535a24b44
niksmac <nikhil@lightrains.com>
2020-09-30 12:41:50 +0530

    Remove unwanted

--- sshalias/install.zsh
+++
@@ -1,7 +0,0 @@
-# ssh-copy-id will not be installed on OSX so
-#
-if test ! $(which ssh-copy-id)
-then
-  echo "Installing ssh-copy-id for you"
-  curl -L https://raw.githubusercontent.com/beautifulcode/ssh-copy-id-for-OSX/master/install.sh | sh
-fi
