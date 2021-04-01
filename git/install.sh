a63daa1232ae23422e6bb59dd674daee9ac1ab06
niksmac <nikhil@lightrains.com>
2021-04-01 09:24:04 +0530

    Ref: lint

--- git/install.sh
+++ git/install.sh
@@ -5,6 +5,8 @@ sudo -v
 
 rm -rf ~/.git-templates/hooks/
 mkdir -p ~/.git-templates/hooks/
+
 git config --global core.hooksPath ~/.git-templates/hooks
+
 yes | cp -rf ~/.dotfiles/git/commit-msg ~/.git-templates/hooks/
 chmod +x ~/.git-templates/hooks/commit-msg
