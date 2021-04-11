bbb0f43a9d1821bb7f5af15096a0b19e9f17d43f
niksmac <nikhil@lightrains.com>
2021-04-11 22:25:43 +0530

    Fix key repeat

--- macos/install.sh
+++ macos/install.sh
@@ -127,7 +127,8 @@ defaults write -g NSAutomaticSpellingCorrectionEnabled -bool true
 defaults write -g ApplePressAndHoldEnabled -bool false
 
 # Sets a very fast repeat rate, adjust to taste.
-defaults write -g KeyRepeat -int 0.1
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
 # Terminal & iTerm 2
 # ==============
