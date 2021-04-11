768823c2f5fefe35a123c36fd9843956b7c1e01a
niksmac <nikhil@lightrains.com>
2021-04-11 09:18:40 +0530

    Fix vscode settings integration

--- vscode/install.sh
+++ vscode/install.sh
@@ -7,12 +7,12 @@ info "Installing vscode plugins"
 
 code --install-extension akamud.vscode-theme-onedark
 code --install-extension amatiasq.sort-imports
-code --install-extension bmalehorn.shell-syntax
 code --install-extension bradlc.vscode-tailwindcss
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension emroussel.atom-icons
 code --install-extension esbenp.prettier-vscode
-code --install-extension ms-vscode.atom-keybindings
 code --install-extension wix.vscode-import-cost
 code --install-extension Wscats.omi-snippets
 
-ln -s "${HOME}/.dotfiles/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
+yes | rm "${HOME}/Library/Application Support/Code/User/settings.json"
+ln -s "${HOME}/.dotfiles/vscode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
