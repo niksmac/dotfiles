a0247a78987c2aa0295fbc25556c16e1e448f1e6
niksmac <nikhil@lightrains.com>
2021-04-24 09:21:19 +0530

    Add ts to prettier beutify cli

--- misc/aliases.zsh
+++ misc/aliases.zsh
@@ -33,7 +33,7 @@ alias pullall='for i in `git remote`; do git pull $i; done;'
 alias spoofmac='sudo ifconfig en0 ether $(openssl rand -hex 6 | sed "s%\(..\)%\1:%g; s%.$%%")'
 
 # Code beautify
-alias prettify="prettier --write '**/*.{ts,js,css,html}'"
+alias prettify="prettier --write '**/*.{ts,js,jsx,css,html}'"
 
 # Confirm before overwriting something
 alias cp="cp -i"
