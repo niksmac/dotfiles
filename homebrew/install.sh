2d9b5ff12d6afd0fa3d6b2da72bf4f2b97493d6d
niksmac <nikhil@lightrains.com>
2021-11-04 08:38:01 +0530

    Change install behavious of brew packages

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -25,7 +25,7 @@ fi
 if test "$(uname)" = "Darwin"
 then
   # Install homebrew packages
-  brew install -s zsh zsh-completions grc coreutils tmux youtube-dl vim node wget git tree tor git-flow wrk zplug yarn imagemagick ffmpeg prettier
+  brew install zsh zsh-completions grc coreutils tmux youtube-dl vim node wget git tree tor git-flow wrk zplug yarn imagemagick ffmpeg prettier
   # Tap cask
   # brew tap homebrew/cask
   # Install cask items
