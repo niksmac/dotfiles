1c04e9cbc1c86b918cf7f11f8c52904c30b73fe9
niksmac <nikhil@lightrains.com>
2022-02-02 23:21:58 +0530

    Add vscode settings

--- vscode/install.sh
+++ vscode/install.sh
@@ -11,10 +11,10 @@ info "Installing vscode plugins"
 code --install-extension akamud.vscode-theme-onedark
 code --install-extension amatiasq.sort-imports
 code --install-extension bradlc.vscode-tailwindcss
+code --install-extension ChakrounAnas.turbo-console-log
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension emroussel.atom-icons
 code --install-extension esbenp.prettier-vscode
-code --install-extension GitHub.copilot
 code --install-extension wix.vscode-import-cost
 code --install-extension Wscats.omi-snippets
 
