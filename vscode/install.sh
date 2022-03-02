13e53ae49b4e9c2510d7d070f7a79a1b3ebe543d
niksmac <nikhil@lightrains.com>
2022-03-02 20:42:09 +0530

    Add new vscode plugins

--- vscode/install.sh
+++ vscode/install.sh
@@ -8,16 +8,18 @@ info "Installing vscode plugins"
 ## Run the below line to get installed plugins list
 # code --list-extensions | xargs -L 1 echo code --install-extension
 
-code --install-extension akamud.vscode-theme-onedark
 code --install-extension amatiasq.sort-imports
-code --install-extension appcypher.astro
 code --install-extension astro-build.astro-vscode
 code --install-extension bradlc.vscode-tailwindcss
+code --install-extension bungcip.better-toml
 code --install-extension ChakrounAnas.turbo-console-log
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension emroussel.atom-icons
+code --install-extension emroussel.atomize-atom-one-dark-theme
 code --install-extension esbenp.prettier-vscode
+code --install-extension rust-lang.rust
 code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension vincaslt.highlight-matching-tag
 code --install-extension wix.vscode-import-cost
 code --install-extension Wscats.omi-snippets
 
