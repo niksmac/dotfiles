858bfb1b4affe9c99118007ba64d8e2849a9b433
niksmac <nikhil@lightrains.com>
2022-05-11 08:38:44 +0530

    Add sort order for install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,27 +1,5 @@
 #!/bin/sh
-#
-# Homebrew
-#
-# This installs some of the common dependencies needed (or at least desired)
-# using Homebrew.
-
-export ARCHFLAGS='-arch arm64'
-
-# Check for Homebrew
-if test ! $(which brew)
-then
-  echo "  Installing Homebrew for you."
-
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
-  fi
-
-fi
+# Install bew deps
 
 # Install the correct homebrew for each OS type
 if test "$(uname)" = "Darwin"
