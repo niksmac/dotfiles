58e9fe6626904b08cdcf4fdf589fbfe198c705e1
niksmac <nikhil@lightrains.com>
2022-12-21 20:28:03 +0530

    Remove cSpell

--- vscode/install.sh
+++ vscode/install.sh
@@ -12,15 +12,15 @@ code --install-extension amatiasq.sort-imports
 code --install-extension astro-build.astro-vscode
 code --install-extension bradlc.vscode-tailwindcss
 code --install-extension bungcip.better-toml
-code --install-extension ChakrounAnas.turbo-console-log
+code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension emroussel.atom-icons
 code --install-extension emroussel.atomize-atom-one-dark-theme
 code --install-extension esbenp.prettier-vscode
 code --install-extension JuanBlanco.solidity
 code --install-extension rust-lang.rust-analyzer
-code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension vincaslt.highlight-matching-tag
+code --install-extension willstakayama.vscode-nextjs-snippets
 code --install-extension wix.vscode-import-cost
 code --install-extension Wscats.omi-snippets
 
