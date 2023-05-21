c74f93aebfcc214b291f08d7a88b4c1d72eea0ab
niksmac <nikhil@lightrains.com>
2023-05-21 12:37:11 +0530

    Add the_silver_searcher

--- homebrew/brew-install.sh
+++ homebrew/brew-install.sh
@@ -5,9 +5,8 @@
 if test "$(uname)" = "Darwin"
 then
   # Install homebrew packages
-  brew install zsh zsh-completions grc coreutils tmux youtube-dl vim node wget git tree tor git-flow-avh wrk zplug yarn imagemagick ffmpeg prettier pwgen rustup
-  # Tap cask
-  # brew tap homebrew/cask
+  brew install zsh zsh-completions grc coreutils tmux youtube-dl vim node wget git tree tor git-flow-avh wrk zplug yarn imagemagick ffmpeg prettier pwgen rustup the_silver_searcher
+
   # Install cask items
   brew install --cask transmission vlc iterm2 firefox visual-studio-code imageoptim slack discord skype insomnia spotify mullvadvpn telegram dropbox exifcleaner keepingyouawake
 elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
