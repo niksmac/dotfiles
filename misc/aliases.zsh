c7390d00d427c8096a625a2bbf2d86ac9f37295a
niksmac <nikhil@lightrains.com>
2023-05-21 11:18:12 +0530

    Remove elm

--- misc/aliases.zsh
+++ misc/aliases.zsh
@@ -12,7 +12,6 @@ alias tree='tree -I node_modules'
 alias v='vim'
 alias speed='speed-test'
 alias ta='tmux a'
-alias epi='elm package install'
 alias lockme='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
 
 # youtube-dl
