697ba7b60d7697b50b07c3fae74da13296afa859
niksmac <nikhil@lightrains.com>
2023-05-30 11:55:29 +0530

    Remove jekyll aliases

--- misc/aliases.zsh
+++ misc/aliases.zsh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Jekyll Awesomeness
-alias js='jekyll clean && jekyll serve'
-alias jb='jekyll build'
-alias jsf='jekyll clean && jekyll serve --future'
 
 alias c='clear'
 alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
