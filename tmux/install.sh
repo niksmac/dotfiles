9d5ebfa3f9f3ff72fc3fb72a5c2c9aeab1a38bfb
niksmac <niksmac@lightrains.com>
2023-07-19 13:49:46 +0530

    Add tpm install for tmux

---
+++ tmux/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# VIM
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+info "Installing tpm for manage tmux plugins!"
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+exit 0
