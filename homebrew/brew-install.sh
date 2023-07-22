7ed00cb2754832466f76473d5dee5352c6e29992
niksmac <niksmac@lightrains.com>
2023-07-22 11:19:07 +0530

    Fix universal brew installer

--- homebrew/brew-install.sh
+++ homebrew/brew-install.sh
@@ -5,10 +5,10 @@
 if test "$(uname)" = "Darwin"
 then
   # Install homebrew packages
-  brew install zsh zsh-completions grc coreutils tmux youtube-dl vim node wget git tree tor git-flow-avh wrk zplug yarn imagemagick ffmpeg prettier pwgen rustup the_silver_searcher
+  brew install zsh zsh-completions grc coreutils tmux youtube-dl vim node wget git tree tor git-flow-avh wrk zplug yarn imagemagick ffmpeg prettier pwgen rustup the_silver_searcher nvm
 
   # Install cask items
-  brew install --cask transmission vlc iterm2 firefox visual-studio-code imageoptim slack discord skype insomnia spotify mullvadvpn telegram dropbox exifcleaner keepingyouawake
+  brew install --cask transmission vlc iterm2 firefox visual-studio-code imageoptim slack discord skype insomnia mullvadvpn telegram dropbox exifcleaner keepingyouawake
 elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
 then
   # Install homebrew packages
