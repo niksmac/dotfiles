8ab3e277a7007537c7a5d468b5f04c33e5d9c606
niksmac <niksmac@lightrains.com>
2023-10-02 18:42:18 +0530

    Add ignorecase git conf

--- git/install.sh
+++ git/install.sh
@@ -9,6 +9,7 @@ mkdir -p ~/.git-templates/hooks/
 git config --global core.hooksPath ~/.git-templates/hooks
 git config --global core.excludesfile ~/.gitignore_global
 git config --global init.defaultBranch master
+git config --global core.ignorecase false
 
 yes | cp -rf ~/.dotfiles/git/commit-msg ~/.git-templates/hooks/
 chmod +x ~/.git-templates/hooks/commit-msg
