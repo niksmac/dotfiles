6dc4d5f5db37e9812873f531c288e78952f84d99
niksmac <niksmac@lightrains.com>
2026-03-19 20:17:28 +0530

    Update install.sh

--- git/install.sh
+++ git/install.sh
@@ -3,6 +3,9 @@
 # Installing commit-msg hook
 sudo -v
 
+# Disable hooks globally
+git config --global core.hooksPath /dev/null
+
 git config --global core.excludesfile ~/.gitignore_global
 git config --global init.defaultBranch master
 git config --global core.ignorecase false
