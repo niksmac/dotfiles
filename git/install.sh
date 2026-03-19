44309102998cf6e70f3e1280f5611156254acab8
niksmac <niksmac@lightrains.com>
2026-03-19 20:15:53 +0530

    Chore: Update git aliases and remove obsolete commit-msg hook
    - Updated alias for empty commit to include "ci:" prefix
    - Removed outdated commit-msg hook and related logic from the project
    - Cleaned up install script by removing redundant commands

--- git/install.sh
+++ git/install.sh
@@ -3,17 +3,6 @@
 # Installing commit-msg hook
 sudo -v
 
-rm -rf ~/.git-templates/hooks/
-mkdir -p ~/.git-templates/hooks/
-
-git config --global core.hooksPath ~/.git-templates/hooks
 git config --global core.excludesfile ~/.gitignore_global
 git config --global init.defaultBranch master
 git config --global core.ignorecase false
-
-# Copy commit-msg hook
-yes | cp -rf ~/.dotfiles/git/commit-msg ~/.git-templates/hooks/
-chmod +x ~/.git-templates/hooks/commit-msg
-
-# Copy imperative verbs file
-yes | cp -rf ~/.dotfiles/git/imperative_verbs.txt ~/.git-templates/hooks/
