90cd9025fe62310c813abdccd950ba0260f5f168
niksmac <niksmac@lightrains.com>
2026-05-24 11:28:31 +0530

    Add prompt formatting

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,93 +1,57 @@
 autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
-
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+branch() {
+  git symbolic-ref --short HEAD 2>/dev/null
 }
 
 git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
+  local b count
+  b=$(branch) || return
+  count=$(git status --porcelain 2>/dev/null | wc -l | tr -d ' ')
+  if [[ $count -gt 0 ]]; then
+    echo "on %{$fg_bold[red]%}$b%{$reset_color%} %{$fg_bold[magenta]%}+$count%{$reset_color%}"
   else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
+    echo "on %{$fg_bold[green]%}$b%{$reset_color%}"
   fi
 }
 
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
-}
-
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
+need_push() {
+  if [[ -n $(git cherry -v @{upstream} 2>/dev/null) ]]; then
     echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+  else
+    echo " "
   fi
 }
 
 node_version() {
-  if (( $+commands[fnm] ))
-  then
-    echo "$(fnm current 2>/dev/null | sed 's/^v//')"
-  elif (( $+commands[node] ))
-  then
-    echo "$(node --version 2>/dev/null | sed 's/^v//')"
-  fi
+  local files=(package.json yarn.lock package-lock.json pnpm-lock.json tsconfig.json)
+  for f in $files; do
+    [[ -f $f ]] && break
+  done && {
+    local v
+    v=$(fnm current 2>/dev/null || node --version 2>/dev/null) && echo " %{$fg_bold[green]%}node@${v#v}%{$reset_color%}"
+  }
 }
 
-python_env() {
-  if [[ -n "$CONDA_DEFAULT_ENV" ]]
-  then
-    echo "conda:$CONDA_DEFAULT_ENV"
-  elif [[ -n "$VIRTUAL_ENV" ]]
-  then
-    echo "venv:$(basename $VIRTUAL_ENV)"
-  elif (( $+commands[pyenv] ))
-  then
-    echo "$(pyenv version-name 2>/dev/null)"
-  fi
-}
-
-runtime_prompt() {
-  local parts=()
-  local nv="$(node_version)"
-  local pv="$(python_env)"
-  [[ -n "$nv" ]] && parts+="%{$fg_bold[green]%}node@$nv%{$reset_color%}"
-  [[ -n "$pv" ]] && parts+="%{$fg_bold[yellow]%}$pv%{$reset_color%}"
-  echo "${(j: :)parts}"
+python_version() {
+  local files=(requirements.txt setup.py setup.cfg pyproject.toml Pipfile)
+  for f in $files; do
+    [[ -f $f ]] && break
+  done && {
+    if [[ -n $CONDA_DEFAULT_ENV ]]; then
+      echo " %{$fg_bold[yellow]%}$CONDA_DEFAULT_ENV%{$reset_color%}"
+    elif [[ -n $VIRTUAL_ENV ]]; then
+      echo " %{$fg_bold[yellow]%}$(basename $VIRTUAL_ENV)%{$reset_color%}"
+    fi
+  }
 }
 
 directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(directory_name) $(git_dirty)$(need_push) $(runtime_prompt)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
-}
+export PROMPT=$'\n$(directory_name) $(git_dirty)$(need_push)$(node_version)$(python_version)\n› '
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
-  set_prompt
 }
